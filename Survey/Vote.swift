5484bba95218b113ba1beadd9f8f1a143fab7c5f
Moin Uddin <U_Moin75@yahoo.com>
2019-04-27 16:18:13 -0400

    Setup Storyboard and Vote Class

---
+++ Survey/Vote.swift
@@ -0,0 +1,21 @@
+//
+//  Vote.swift
+//  Survey
+//
+//  Created by Moin Uddin on 4/27/19.
+//  Copyright © 2019 Moin Uddin. All rights reserved.
+//
+
+import Foundation
+
+
+class Vote {
+    var name: String
+    var response: String
+    
+    
+    init(name: String, response: String) {
+        self.name = name
+        self.response = response
+    }
+}
